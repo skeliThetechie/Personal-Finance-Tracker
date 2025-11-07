c9f03ed62e659bfd5d6fb4d885bba2435d76e2fd
skeliThetechie <jlharvey30@gmail.com>
2025-11-07 18:21:36 -0500

    frontend development of login page

--- clientApp/src/App.js
+++ clientApp/src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      <div className="login-card">
+        <h1 className="App-title">Personal Finance Tracker</h1>
+          <ToastContainer position="top-center" autoClose={2000} />
           <Login/>
-          Edit <code>src/App.js</code> and save to reload. Test 444 hello try test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
--- clientApp/src/pages/Login.js
+++ clientApp/src/pages/Login.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { TextField, Grid } from "@mui/material";
+import { toast } from "react-toastify";
 
 const Login = () => {
     const {
@@ -7,8 +9,67 @@ const Login = () => {
         handleSubmit,
         watch,
         formState: { errors },
-    } = useForm;
-    return <div>Login</div>;
-};  
+    } = useForm();
+
+    const onSubmit = (data) => {
+        const username = watch("username")?.trim();
+        const password = watch("password")?.trim();
+
+        console.log("Submitted:", { username, password });
+
+        if (!username || !password) {
+            toast.error("Username and password cannot be empty.");
+            return;
+        }
+
+        toast.success("Successfully Logged In!");
+    };
+
+    console.log("Username: ", watch("username"));
+    console.log("Password: ", watch("password"));
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Grid container spacing={1}>
+                <Grid size={{ xs: 12 }}>
+                    <TextField
+                        name="username"
+                        required
+                        id="outlined-required"
+                        label="Username"
+                        {...register("username", {
+                            required: "Username is required",
+                            validate: (value) => value.trim() !== "" || "Username cannot be just spaces",
+                        })}
+                        error={!!errors.username}
+                        helperText={errors.username?.message}
+                    />
+                </Grid>
+                <Grid size={{ xs: 6 }} />
+                <Grid size={{ xs: 12 }}>
+                    <TextField
+                        name="password"
+                        required
+                        {...register("password")}
+                        id="outlined-password-input"
+                        label="Password"
+                        type="password"
+                        autoComplete="current-password"
+                        {...register("password", {
+                            required: "Password is required",
+                            validate: (value) => value.trim() !== "" || "Password cannot be just spaces",
+                        })}
+                        error={!!errors.password}
+                        helperText={errors.password?.message}
+                    />
+                </Grid>
+                <Grid size={{ xs: 6 }} />
+                <Grid size={{ xs: 12 }}>
+                    <input type="submit" />
+                </Grid>
+            </Grid>
+        </form>
+    );
+};
 
 export default Login;
